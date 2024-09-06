b1cee7303aa01fe3ccb3f4bc2aad76600a4c84a0
wanderson <wandersondacostacorrea133@gmail.com>
2024-09-06 08:54:06 -0300

    implement sign on jwt

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -5,13 +5,13 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
 
   const token = await reply.jwtSign({
     sign: {
-      sub: request.user.sub,
+      sub: request.user.sign.sub,
     },
   })
 
   const refreshToken = await reply.jwtSign({
     sign: {
-      sub: request.user.sub,
+      sub: request.user.sign.sub,
       expiresIn: '7d',
     },
   })
